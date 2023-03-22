e1b2f8f1cdbd6fedebed09eab662a7fa1fe36cc7
lib26 <dladlsqja@naver.com>
2023-03-22 23:49:53 +0900

    domain docs

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,13 +15,17 @@ import java.util.List;
 @Getter
 @Setter
 public class Member {
+
     @Id
     @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "member_id") // 이렇게 테이블 컬럼 값 설정을 안해주면, 관례상 id라고 자동으로 컬럼이름이 매핑된다.
     private Long id;
+
     private String name;
-    @Embedded
+
+    @Embedded // 내장 타입이 내장되었다는 의미
     private Address address;
-    @OneToMany(mappedBy = "member")
+
+    @OneToMany(mappedBy = "member") // Member 테이블 입장에서 Orders 테이블은 일대다 관계이다.
     private List<Order> orders = new ArrayList<>();
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,23 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
+@Table(name = "orders") // 이렇게 테이블 명을 지정해주지 않으면 관례상 order로 자동 세팅된다.
 @Getter
 @Setter
 public class Order {
+
     @Id
     @GeneratedValue
     @Column(name = "order_id")
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+
+    @ManyToOne(fetch = FetchType.LAZY) // orders 테이블 입장에서는 member 테이블과 다대일 관계이다
+    @JoinColumn(name = "member_id") // FK 이름 매핑
     private Member member; //주문 회원
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
+
     private LocalDateTime orderDate; //주문시간
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 

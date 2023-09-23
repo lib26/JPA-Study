8626772544cf65eff020cd0bc81cc23b00d119f5
lib26 <dladlsqja@naver.com>
2023-09-23 17:42:54 +0900

    feat: 조회용 셈플 데이터 입력

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -29,6 +29,8 @@ public class Member {
     // Member 테이블 입장에서 Orders 테이블은 일대다 관계이다.
     // Order 테이블에 있는 member 필드 값에 의해서 매핑 된다는 의미
     // 즉, 맴버와 주문 사이의 연관 관계 주인은 주문 테이블이 된다.
+    // 근데 이런식으로 Member에 굳이 orders list가 있는건 약간 잘못된 설계임. 예제니까 넣는거임.
+    // 이럴때는 관심사를 끊어내기 위해 order 테이블에서 단방향 매핑만 해주면 됨
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
